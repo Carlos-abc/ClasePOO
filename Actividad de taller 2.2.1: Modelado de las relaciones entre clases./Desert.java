ded6e552dca84094a8f070d8539f6741a35016e8
Carlos-abc <72826660+Carlos-abc@users.noreply.github.com>
2022-09-24 20:26:34 -0700

    Add files via upload

---
+++ Actividad de taller 2.2.1: Modelado de las relaciones entre clases./Desert.java
@@ -0,0 +1,12 @@
+public class Desert {
+	private String _name;
+	public Bioma _unnamed_Bioma_;
+
+	public void getName() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setName(Object aName) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ Actividad de taller 2.2.1: Modelado de las relaciones entre clases./Plant.java
@@ -0,0 +1,12 @@
+public class Plant {
+	private String _name;
+	public Organism _unnamed_Organism_;
+
+	public void getName() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setName(Object aName) {
+		throw new UnsupportedOperationException();
+	}
+}

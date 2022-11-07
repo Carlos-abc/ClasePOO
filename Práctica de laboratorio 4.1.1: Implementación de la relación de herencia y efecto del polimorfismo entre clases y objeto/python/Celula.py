d9a3f026665777fda733af85f6d0bbc87d27c768
Carlos-abc <72826660+Carlos-abc@users.noreply.github.com>
2022-11-06 16:13:47 -0800

    Add files via upload

---
+++ Práctica de laboratorio 4.1.1: Implementación de la relación de herencia y efecto del polimorfismo entre clases y objeto/python/Celula.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+class Celula(object):
+	def getNombre(self):
+		return self.___nombre
+
+	def setNombre(self, aNombre):
+		"""@ReturnType String"""
+		pass
+
+	def getTipo(self):
+		return self.___tipo
+
+	def setTipo(self, aTipo):
+		"""@ReturnType String"""
+		pass
+
+	def mostrarDatos(self):
+		"""@ReturnType String"""
+		pass
+
+	def __init__(self):
+		self.___nombre = None
+		self.___tipo = None
+
---
+++ Práctica de laboratorio 4.1.1: Implementación de la relación de herencia y efecto del polimorfismo entre clases y objeto/python/Ovulo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import Celula
+
+class Ovulo(Celula):
+	def mostrarDatos(self):
+		"""@ReturnType String"""
+		pass
+
